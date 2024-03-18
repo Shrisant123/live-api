60bbe10619956e62bef8522e3927fe3531168390
shreesant adhikari <shreesantadhikari4590@gmail.com>
2024-03-18 09:44:09 +0545

    live api

---
+++ app.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const app=express();
+port=process.env.PORT || 3000;
+
+app.get('/home', (req,res)=>{
+	res.send("This is home page");
+})
+
+app.get('/stopwatch', (req,res)=>{
+	res.sendFile('index.html', {root:__dirname});
+})
+
+app.get('/Eproducts', (req,res)=>{
+	res.sendFile('Allproducts.json', {root:__dirname});
+})
+
+app.get('/ItemProducts', (req,res)=>{
+	res.sendFile('items.json', {root:__dirname});
+})
+
+app.listen(port, ()=>{
+	console.log(`The port is listening at ${port}`);
+})
+
+
+
